9862dabec57c294d9377f30fa861c79513b9bbce
Anumeti <108921379+Anumeti@users.noreply.github.com>
2024-01-06 13:45:52 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main()
+{   //This the code for perimeter of rectangle
+
+    int length;
+    printf("write the length");
+    scanf("%d",&length);
+
+    int width;
+    printf("write the width");
+    scanf("%d",&width);
+
+    printf("The perimeter of triangle is : %d\n", (length+width)*2 );
+    return 0;
+
+    }
